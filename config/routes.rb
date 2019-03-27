dfbb8f891e24a8b0eb9fa6f8b09c5ae8568f4277
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-27 23:23:02 +0000

    No idea what the hell I am doing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
  root 'static_pages#index'
 <<<<<<< HEAD
+<<<<<<< HEAD
  resources :courses, only: [:index, :show]
+=======
+>>>>>>> parent of 44f6d3c... I lost functionality in my navbar
  namespace :instructor do 
  	resources :courses, only: [:new, :create, :show]
  	
