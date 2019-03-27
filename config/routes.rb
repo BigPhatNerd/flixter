fcfc86b06c1324c17846014966ec4c59c6db029b
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-27 23:08:47 +0000

    Trying to revert to a previous commit and think I probably messed it up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
  root 'static_pages#index'
+<<<<<<< HEAD
  resources :courses, only: [:index, :show]
  namespace :instructor do 
  	resources :courses, only: [:new, :create, :show]
  	
  end
+=======
+>>>>>>> parent of 761f8de... Added instructor form for courses
 end
